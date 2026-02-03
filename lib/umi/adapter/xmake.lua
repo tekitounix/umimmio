a22e76e9f4d9964515f62bfb0c160385298f2c35
tekitounix <sicksteen@me.com>
2026-02-04 03:33:02 +0900

    feat(umi): migrate adapter to umi/adapter

---
+++ lib/umi/adapter/xmake.lua
@@ -0,0 +1,20 @@
+add_rules("mode.debug", "mode.release")
+
+-- Target definition
+local target_name = "umi_adapter"
+target(target_name)
+    set_kind("static")
+    add_rules("coding.umi_library")
+    
+    -- Include directory
+    add_includedirs(".", {public = true})
+    
+    -- Header files only (header-only library)
+    add_headerfiles("*.hh")
+    add_headerfiles("web/*.hh")
+    add_headerfiles("web/*.js")
+    
+    -- Dependencies
+    add_deps("umi_core", "umi_kernel")
+    
+target_end()
