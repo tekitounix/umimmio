5402143ba034c497e4848a69507dba86cf94fdac
tekitounix <sicksteen@me.com>
2026-02-04 02:51:42 +0900

    feat(umi): migrate service libraries to umi/service
    Migrate System Service from lib/umios/kernel/ to lib/umi/service/:
    - loader/ (app loader)
    - shell/ (SysEx shell)
    - audio/ (audio service)
    - midi/ (midi service)
    - storage/ (storage service)
    
    Part of Phase 1 Step 2.5: library unification

---
+++ lib/umi/service/xmake.lua
@@ -0,0 +1,18 @@
+add_rules("mode.debug", "mode.release")
+
+target("umi_service")
+    set_kind("static")
+    add_rules("coding.umi_library")
+    add_includedirs(".", {public = true})
+    
+    -- Source files
+    add_files("loader/*.cc")
+    add_headerfiles("loader/*.hh")
+    add_headerfiles("shell/*.hh")
+    add_headerfiles("audio/*.hh")
+    add_headerfiles("midi/*.hh")
+    add_headerfiles("storage/*.hh")
+    
+    -- Dependencies
+    add_deps("umi_core", "umi_runtime", "umi_fs")
+target_end()
